571dd907cda97eac7d2914e86dcbc95c1fc81900
Mir <113521253+Mirfirdavs@users.noreply.github.com>
2023-04-24 22:31:49 +0300

    connecting database

--- app.py
+++ app.py
@@ -1,15 +1,42 @@
+import sqlite3
+import os
 from flask import Flask, render_template, url_for, request, flash, redirect, session, abort
 
-#Это ветка log-in
+# конфигурация
+DATABASE = '/tmp/sauna.db'
+DEBUG = True
+SECRET_KEY = 'fawheoilnfweaughsaweogihanfk'
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'fawheoilnfweaughsaweogihanfk'
+app.config.from_object(__name__)
+
+#переопределение пути к базе данных
+app.config.update(dict(DATABASE=os.path.join(app.root_path, 'sauna.db')))
+
+#Общая функция для соеденеия с базой данных
+def connect_db():
+    conn = sqlite3.connect(app.config['DATABASE'])
+    conn.row_factory = sqlite3.Row
+    return conn
+
+#Создает базу данных с набором таблиц
+def create_db():
+    '''Вспомогательная функция для создания таблиц БД'''
+    db = connect_db()
+    with app.open_resource('sq_db.sgl', mode = 'r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+    db.close()
+
 
 @app.route('/index')
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
+#Это ветка log-in
 
 @app.route('/contact', methods = ['GET', 'POST'])
 def contact():
