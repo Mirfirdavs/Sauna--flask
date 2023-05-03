ff5efc097496adc77fb249f94bf9c583742d8796
Mir <113521253+Mirfirdavs@users.noreply.github.com>
2023-05-04 00:23:21 +0300

    Added db

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import sqlite3
 import os
-from flask import Flask, render_template, url_for, request, flash, redirect, session, abort
+from flask import Flask, render_template, url_for, request, flash, redirect, session, abort, g
 
 # конфигурация
 DATABASE = '/tmp/sauna.db'
@@ -9,7 +9,7 @@ SECRET_KEY = 'fawheoilnfweaughsaweogihanfk'
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'fawheoilnfweaughsaweogihanfk'
-app.config.from_object(__name__)
+app.config.from_object(__name__) #из модуля загружается "Конфигурация"
 
 #переопределение пути к базе данных
 app.config.update(dict(DATABASE=os.path.join(app.root_path, 'sauna.db')))
@@ -24,17 +24,30 @@ def connect_db():
 def create_db():
     '''Вспомогательная функция для создания таблиц БД'''
     db = connect_db()
-    with app.open_resource('sq_db.sgl', mode = 'r') as f:
+    with app.open_resource('sq_db.sql', mode = 'r') as f:
         db.cursor().executescript(f.read())
     db.commit()
     db.close()
 
 
+def get_db():
+    '''Соединение с БД, если оно еще не установлено'''
+    if not hasattr(g, 'link_db'):
+        g.link_db = connect_db()
+    return g.link_db
+
+
 @app.route('/index')
 @app.route('/')
 def index():
+    db = get_db()
     return render_template('index.html')
 
+@app.teardown_appcontext
+def close_db(error):
+    '''Закрываем соединение с БД, если оно было установлено'''
+    if hasattr(g, 'link_db'):
+        g.link_db.close()
 
 #Это ветка log-in
 
